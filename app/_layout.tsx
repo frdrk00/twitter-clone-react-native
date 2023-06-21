a617740648736cca1ee4b529ae63b8c4bfd4d74c
frdrk00 <masterkooo@gmail.com>
2023-06-21 18:12:03 +0200

    New Tweet Page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,6 +49,10 @@ function RootLayoutNav() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen
+            name="new-tweet"
+            options={{ title: 'New Tweet', headerShown: false }}
+          />
           <Stack.Screen name="tweet/[id]" options={{ title: 'Tweet' }} />
         </Stack>
       </ThemeProvider>
